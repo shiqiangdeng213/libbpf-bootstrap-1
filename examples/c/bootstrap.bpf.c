8b8dd40d26f068af9fe5be6d75ff4b43367fd5d6
shiqiang.deng213 <shiqiang.deng213@gmail.com>
2026-03-13 11:41:23 +0800

    【增加bootstrap备注】:增加bootstrap备注
    增加bootstrap备注

--- examples/c/bootstrap.bpf.c
+++ examples/c/bootstrap.bpf.c
@@ -8,13 +8,14 @@
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
+/* 定义哈希表，用来存放进程的启动信息 */
 struct {
 	__uint(type, BPF_MAP_TYPE_HASH);
-	__uint(max_entries, 8192);
-	__type(key, pid_t);
-	__type(value, u64);
+	__uint(max_entries, 8192);			/* 系统中应该不会同时存在8192个正在运行的进程 */
+	__type(key, pid_t);					/* 进程id作为键 */
+	__type(value, u64);					/* 启动时候的时间戳作为值 */
 } exec_start SEC(".maps");
-
+/* 定义ringbuf用于像用户空间传递数据 */
 struct {
 	__uint(type, BPF_MAP_TYPE_RINGBUF);
 	__uint(max_entries, 256 * 1024);
@@ -27,37 +28,37 @@ int handle_exec(struct trace_event_raw_sched_process_exec *ctx)
 {
 	struct task_struct *task;
 	unsigned fname_off;
-	struct event *e;
+	struct event *e;										/* 自定义的结构体用来描述进程信息 */
 	pid_t pid;
 	u64 ts;
 
 	/* remember time exec() was executed for this PID */
-	pid = bpf_get_current_pid_tgid() >> 32;
-	ts = bpf_ktime_get_ns();
-	bpf_map_update_elem(&exec_start, &pid, &ts, BPF_ANY);
+	pid = bpf_get_current_pid_tgid() >> 32;					/* 调用sched_process_exec函数的进程id */
+	ts = bpf_ktime_get_ns();								/* 调用函数时候的时间戳 */
+	bpf_map_update_elem(&exec_start, &pid, &ts, BPF_ANY);	/* 记录进程id和调用时候的时间戳到map */
 
 	/* don't emit exec events when minimum duration is specified */
 	if (min_duration_ns)
 		return 0;
 
 	/* reserve sample from BPF ringbuf */
-	e = bpf_ringbuf_reserve(&rb, sizeof(*e), 0);
+	e = bpf_ringbuf_reserve(&rb, sizeof(*e), 0);			/* 申请ringbuf */
 	if (!e)
 		return 0;
 
 	/* fill out the sample with data */
-	task = (struct task_struct *)bpf_get_current_task();
+	task = (struct task_struct *)bpf_get_current_task();	/* 获取调用这个进程的任务结构体 */
 
-	e->exit_event = false;
-	e->pid = pid;
-	e->ppid = BPF_CORE_READ(task, real_parent, tgid);
-	bpf_get_current_comm(&e->comm, sizeof(e->comm));
+	e->exit_event = false;									/* 置位flase表示进程还没退出 */
+	e->pid = pid;											/* 记录进程id */
+	e->ppid = BPF_CORE_READ(task, real_parent, tgid);		/* 获取父进程id */
+	bpf_get_current_comm(&e->comm, sizeof(e->comm));		/* 获取进程名字，将名字拷贝到e->comm数组中 */
 
 	fname_off = ctx->__data_loc_filename & 0xFFFF;
-	bpf_probe_read_str(&e->filename, sizeof(e->filename), (void *)ctx + fname_off);
+	bpf_probe_read_str(&e->filename, sizeof(e->filename), (void *)ctx + fname_off);	/* 获取文件路径，比如:/usr/bin/grep */
 
 	/* successfully submit it to user-space for post-processing */
-	bpf_ringbuf_submit(e, 0);
+	bpf_ringbuf_submit(e, 0);								/* 进程开始执行的时候把数据提交到用户空间 */
 	return 0;
 }
 
@@ -70,42 +71,42 @@ int handle_exit(struct trace_event_raw_sched_process_template *ctx)
 	u64 id, ts, *start_ts, duration_ns = 0;
 
 	/* get PID and TID of exiting thread/process */
-	id = bpf_get_current_pid_tgid();
-	pid = id >> 32;
-	tid = (u32)id;
+	id = bpf_get_current_pid_tgid();						/* 获取线程id */
+	pid = id >> 32;											/* 获取进程id */
+	tid = (u32)id;											/* 线程id */
 
 	/* ignore thread exits */
-	if (pid != tid)
+	if (pid != tid)											/* 判断退出的是否是线程，是线程的话不记录 */
 		return 0;
 
 	/* if we recorded start of the process, calculate lifetime duration */
-	start_ts = bpf_map_lookup_elem(&exec_start, &pid);
+	start_ts = bpf_map_lookup_elem(&exec_start, &pid);		/* 通过进程id去查找map，看是否有过开始执行的记录 */
 	if (start_ts)
-		duration_ns = bpf_ktime_get_ns() - *start_ts;
+		duration_ns = bpf_ktime_get_ns() - *start_ts;		/* 当前的时间戳减去开始执行的时间戳得到进程执行的时长 */
 	else if (min_duration_ns)
 		return 0;
-	bpf_map_delete_elem(&exec_start, &pid);
+	bpf_map_delete_elem(&exec_start, &pid);					/* 进程已经退出了，删除对应的map */
 
 	/* if process didn't live long enough, return early */
-	if (min_duration_ns && duration_ns < min_duration_ns)
+	if (min_duration_ns && duration_ns < min_duration_ns)	/* 运行的间隔如果小于设置的间隔，就不记录了 */
 		return 0;
 
 	/* reserve sample from BPF ringbuf */
-	e = bpf_ringbuf_reserve(&rb, sizeof(*e), 0);
+	e = bpf_ringbuf_reserve(&rb, sizeof(*e), 0);			/* 申请ringbuf */
 	if (!e)
 		return 0;
 
 	/* fill out the sample with data */
-	task = (struct task_struct *)bpf_get_current_task();
+	task = (struct task_struct *)bpf_get_current_task();	/* 拿到调用这个sched_process_exit的任务结构体 */
 
-	e->exit_event = true;
-	e->duration_ns = duration_ns;
-	e->pid = pid;
-	e->ppid = BPF_CORE_READ(task, real_parent, tgid);
-	e->exit_code = (BPF_CORE_READ(task, exit_code) >> 8) & 0xff;
-	bpf_get_current_comm(&e->comm, sizeof(e->comm));
+	e->exit_event = true;									/* 标记进程已经退出了 */
+	e->duration_ns = duration_ns;							/* 记录总的运行时长 */
+	e->pid = pid;											/* 记录进程id */
+	e->ppid = BPF_CORE_READ(task, real_parent, tgid);		/* 获取父进程id */
+	e->exit_code = (BPF_CORE_READ(task, exit_code) >> 8) & 0xff;	/* 进程的退出码 */
+	bpf_get_current_comm(&e->comm, sizeof(e->comm));		/* 获取进程的comm */
 
 	/* send data to user-space for post-processing */
-	bpf_ringbuf_submit(e, 0);
+	bpf_ringbuf_submit(e, 0);								/* 进程退出的时候把数据提交到用户空间 */
 	return 0;
 }
--- examples/c/bootstrap.c
+++ examples/c/bootstrap.c
@@ -8,7 +8,7 @@
 #include <bpf/libbpf.h>
 #include "bootstrap.h"
 #include "bootstrap.skel.h"
-
+/* 声明一个结构体，同时定义一个结构体变量 */
 static struct env {
 	bool verbose;
 	long min_duration_ms;
@@ -78,18 +78,21 @@ static int handle_event(void *ctx, void *data, size_t data_sz)
 	struct tm *tm;
 	char ts[32];
 	time_t t;
-
+	/* 拿到系统时间 */
 	time(&t);
 	tm = localtime(&t);
 	strftime(ts, sizeof(ts), "%H:%M:%S", tm);
-
+	/* 判断是退出事件还是执行事件 */
 	if (e->exit_event) {
+		/* 是进程退出事件，打印进程名字，进程id，父进程id，退出码 */
 		printf("%-8s %-5s %-16s %-7d %-7d [%u]", ts, "EXIT", e->comm, e->pid, e->ppid,
 		       e->exit_code);
+		/* 打印间隔 */
 		if (e->duration_ns)
 			printf(" (%llums)", e->duration_ns / 1000000);
 		printf("\n");
 	} else {
+		/* 是进程运行事件，打印进程的完整路径名 */
 		printf("%-8s %-5s %-16s %-7d %-7d %s\n", ts, "EXEC", e->comm, e->pid, e->ppid,
 		       e->filename);
 	}
@@ -99,46 +102,46 @@ static int handle_event(void *ctx, void *data, size_t data_sz)
 
 int main(int argc, char **argv)
 {
-	struct ring_buffer *rb = NULL;
-	struct bootstrap_bpf *skel;
+	struct ring_buffer *rb = NULL;	/* 描述一个ringbuf */
+	struct bootstrap_bpf *skel;		/* 手脚架文件 */
 	int err;
 
 	/* Parse command line arguments */
-	err = argp_parse(&argp, argc, argv, 0, NULL, NULL);
+	err = argp_parse(&argp, argc, argv, 0, NULL, NULL);	/* 解析命令行参数 */
 	if (err)
 		return err;
 
 	/* Set up libbpf errors and debug info callback */
 	libbpf_set_print(libbpf_print_fn);
-
+	/* 命令行的信号处理 */
 	/* Cleaner handling of Ctrl-C */
 	signal(SIGINT, sig_handler);
 	signal(SIGTERM, sig_handler);
 
 	/* Load and verify BPF application */
-	skel = bootstrap_bpf__open();
+	skel = bootstrap_bpf__open();						/* 打开bpf文件 */
 	if (!skel) {
 		fprintf(stderr, "Failed to open and load BPF skeleton\n");
 		return 1;
 	}
-
+	/* 将应用层设置进来的最小时间间隔设置到bpf内核文件中去 */
 	/* Parameterize BPF code with minimum duration parameter */
-	skel->rodata->min_duration_ns = env.min_duration_ms * 1000000ULL;
+	skel->rodata->min_duration_ns = env.min_duration_ms * 1000000ULL;	/* 拿到bpf文件中的全局变量min_duration_ns，然后赋值 */
 
 	/* Load & verify BPF programs */
-	err = bootstrap_bpf__load(skel);
+	err = bootstrap_bpf__load(skel);	/* 加载到内核中进行字节码校验 */
 	if (err) {
 		fprintf(stderr, "Failed to load and verify BPF skeleton\n");
 		goto cleanup;
 	}
 
 	/* Attach tracepoints */
-	err = bootstrap_bpf__attach(skel);
+	err = bootstrap_bpf__attach(skel);	/* 挂载跟踪点 */
 	if (err) {
 		fprintf(stderr, "Failed to attach BPF skeleton\n");
 		goto cleanup;
 	}
-
+	/* 设置唤醒缓冲区轮询 */
 	/* Set up ring buffer polling */
 	rb = ring_buffer__new(bpf_map__fd(skel->maps.rb), handle_event, NULL, NULL);
 	if (!rb) {
@@ -146,11 +149,13 @@ int main(int argc, char **argv)
 		fprintf(stderr, "Failed to create ring buffer\n");
 		goto cleanup;
 	}
-
+	/* 设置打印标题 */
 	/* Process events */
 	printf("%-8s %-5s %-16s %-7s %-7s %s\n", "TIME", "EVENT", "COMM", "PID", "PPID",
 	       "FILENAME/EXIT CODE");
+	/* 没有收到退出信号就持续轮询 */
 	while (!exiting) {
+		/* 调用ringbuf轮询函数，超时设置为100ms，然后去执行 handle_event */
 		err = ring_buffer__poll(rb, 100 /* timeout, ms */);
 		/* Ctrl-C will cause -EINTR */
 		if (err == -EINTR) {
