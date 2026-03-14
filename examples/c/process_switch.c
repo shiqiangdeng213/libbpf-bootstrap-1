ba3fa5670e6b5a7e0f8519afbf0bd0c745d1f2a0
shiqiang.deng213 <shiqiang.deng213@gmail.com>
2026-03-14 14:27:21 +0800

    【process_switch】:注释
    注释

--- examples/c/process_switch.c
+++ examples/c/process_switch.c
@@ -8,63 +8,6 @@
 #include <bpf/libbpf.h>
 #include "process_switch.h"
 #include "process_switch.skel.h"
-/* 声明一个结构体，同时定义一个结构体变量 */
-/*
-static struct env {
-	bool verbose;
-	long min_duration_ms;
-} env;
-*/
-//const char *argp_program_version = "bootstrap 0.0";
-//const char *argp_program_bug_address = "<bpf@vger.kernel.org>";
-//const char argp_program_doc[] = "BPF bootstrap demo application.\n"
-//				"\n"
-//				"It traces process start and exits and shows associated \n"
-//				"information (filename, process duration, PID and PPID, etc).\n"
-//				"\n"
-//				"USAGE: ./bootstrap [-d <min-duration-ms>] [-v]\n";
-//
-//static const struct argp_option opts[] = {
-//	{ "verbose", 'v', NULL, 0, "Verbose debug output" },
-//	{ "duration", 'd', "DURATION-MS", 0, "Minimum process duration (ms) to report" },
-//	{},
-//};
-//
-//static error_t parse_arg(int key, char *arg, struct argp_state *state)
-//{
-//	switch (key) {
-//	case 'v':
-//		env.verbose = true;
-//		break;
-//	case 'd':
-//		errno = 0;
-//		env.min_duration_ms = strtol(arg, NULL, 10);
-//		if (errno || env.min_duration_ms <= 0) {
-//			fprintf(stderr, "Invalid duration: %s\n", arg);
-//			argp_usage(state);
-//		}
-//		break;
-//	case ARGP_KEY_ARG:
-//		argp_usage(state);
-//		break;
-//	default:
-//		return ARGP_ERR_UNKNOWN;
-//	}
-//	return 0;
-//}
-//
-//static const struct argp argp = {
-//	.options = opts,
-//	.parser = parse_arg,
-//	.doc = argp_program_doc,
-//};
-//
-//static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args)
-//{
-//	if (level == LIBBPF_DEBUG && !env.verbose)
-//		return 0;
-//	return vfprintf(stderr, format, args);
-//}
 
 static volatile bool exiting = false;
 
@@ -83,21 +26,7 @@ static int handle_event(void *ctx, void *data, size_t data_sz)
 	time(&t);
 	tm = localtime(&t);
 	strftime(ts, sizeof(ts), "%H:%M:%S", tm);
-	/* 判断是退出事件还是执行事件 */
-	//if (e->exit_event) {
-		/* 是进程退出事件，打印进程名字，进程id，父进程id，退出码 */
-	//	printf("%-8s %-5s %-16s %-7d %-7d [%u]", ts, "EXIT", e->comm, e->pid, e->ppid,
-	//	       e->exit_code);
-		/* 打印间隔 */
-	//	if (e->duration_ns)
-	//		printf(" (%llums)", e->duration_ns / 1000000);
-	//	printf("\n");
-	//} else {
-		/* 是进程运行事件，打印进程的完整路径名 */
-	//	printf("%-8s %-5s %-16s %-7d %-7d %s\n", ts, "EXEC", e->comm, e->pid, e->ppid,
-	//	       e->filename);
-	//}
-
+	
 	printf("%-8s\t %-5d\t\t %-16s\t %-7d\t %-7s\t\t %ld\n", ts, e->prev_pid, e->prev_comm, e->next_pid, e->next_comm, e->runtime);
 
 	return 0;
@@ -105,32 +34,21 @@ static int handle_event(void *ctx, void *data, size_t data_sz)
 
 int main(int argc, char **argv)
 {
-	struct ring_buffer *rb = NULL;	/* 描述一个ringbuf */
+	struct ring_buffer *rb = NULL;			/* 描述一个ringbuf */
 	struct process_switch_bpf *skel;		/* 手脚架文件 */
 	int err;
-
-	/* Parse command line arguments */
-	//err = argp_parse(&argp, argc, argv, 0, NULL, NULL);	/* 解析命令行参数 */
-	//if (err)
-	//	return err;
-
-	/* Set up libbpf errors and debug info callback */
-	//libbpf_set_print(libbpf_print_fn);
-	/* 命令行的信号处理 */
+	
 	/* Cleaner handling of Ctrl-C */
 	signal(SIGINT, sig_handler);
 	signal(SIGTERM, sig_handler);
 
 	/* Load and verify BPF application */
-	skel = process_switch_bpf__open();						/* 打开bpf文件 */
+	skel = process_switch_bpf__open();		/* 打开bpf文件 */
 	if (!skel) {
 		fprintf(stderr, "Failed to open and load BPF skeleton\n");
 		return 1;
 	}
-	/* 将应用层设置进来的最小时间间隔设置到bpf内核文件中去 */
-	/* Parameterize BPF code with minimum duration parameter */
-	//skel->rodata->min_duration_ns = env.min_duration_ms * 1000000ULL;	/* 拿到bpf文件中的全局变量min_duration_ns，然后赋值 */
-
+	
 	/* Load & verify BPF programs */
 	err = process_switch_bpf__load(skel);	/* 加载到内核中进行字节码校验 */
 	if (err) {
--- examples/c/process_switch.h
+++ examples/c/process_switch.h
@@ -18,16 +18,5 @@ struct process_info {
 	unsigned long runtime;			/* 两次调度之间的时间间隔 */
 };
 
-/*
-struct event {
-	int pid;
-	int ppid;
-	unsigned exit_code;
-	unsigned long long duration_ns;
-	char comm[TASK_COMM_LEN];
-	char filename[MAX_FILENAME_LEN];
-	bool exit_event;
-};
-*/
 #endif /* __BOOTSTRAP_H */
 
