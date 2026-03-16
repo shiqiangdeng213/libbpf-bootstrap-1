f3a35e43a8bd41a2da2bbc8283ecf20b68de4460
shiqiang.deng213 <shiqiang.deng213@gmail.com>
2026-03-16 14:09:40 +0800

    【migrate_monitor】:进程名16字节
    进程名16字节

--- examples/c/migrate_monitor.h
+++ examples/c/migrate_monitor.h
@@ -3,7 +3,7 @@
 
 struct event {
     pid_t pid;
-    char comm[32];			// 内核线程来说好像32个不够
+    char comm[16];			// 内核线程来说好像32个不够
     int orig_cpu;			// 当前cpu
     int dest_cpu;
     int src_nid;			// 当前numa节点
